2018c80590feea1518d97a9aed08201a06ae50f4
Meera <mjvsharma@outlook.com>
2017-09-26 13:39:20 -0400

    laid out framework for class zombie

---
+++ zombie.rb
@@ -0,0 +1,52 @@
+class Zombie
+
+@@horde = []
+@@plague_level = 10
+@@max_speed = 5
+@@max_strength = 8
+@@default_speed = 1
+@@default_strength = 3
+
+def initialize(zombie_speed, zombie_strengths)
+
+  if zombie_speed > @@max_speed
+    
+end
+
+
+def encounter
+
+end
+
+def outrun_zombie?
+
+end
+
+def survive_attack?
+
+end
+
+def self.all
+
+end
+
+def self.new_day
+
+end
+
+def self.some_die_off
+
+end
+
+def self.spawn
+
+end
+
+def self.increase_plague_level
+
+end
+
+
+
+
+end
