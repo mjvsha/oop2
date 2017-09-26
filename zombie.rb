d5dfde9d77bfde4905e14acd4f87bbb30f5efaf7
Meera <mjvsharma@outlook.com>
2017-09-26 17:19:54 -0400

    i think its working but i want to run inspect on idividually created zombies to be sure

--- zombie.rb
+++ zombie.rb
@@ -1,5 +1,7 @@
 class Zombie
 
+  #how to update @@plague_level
+
 @@horde = []
 @@plague_level = 10
 @@max_speed = 5
@@ -26,14 +28,14 @@ end
 def encounter
   #escape
 
-  if self.outrun_zombie == true
+  if self.outrun_zombie? == true
     puts "YOU LIVE TO SEE ANOTHER DAY"
-  elsif self.outrun_zombie == false
+  elsif self.outrun_zombie? == false
     puts "Shit you are not able to outrun the zombie !!! You have to fight bitch!!"
     if self.survive_attack? == false
       puts "DAMN....YOU DIED"
     else
-      you_become_one = Zombie.new(rand(@@max_speed), @@max_strength)
+      you_become_one = Zombie.new(rand(@@max_speed), rand(@@max_strength))
       @@horde << you_become_one
       puts "YOU BECOME A ZOMBIE"
     end
@@ -42,13 +44,7 @@ def encounter
 end
 
 
-  #get killed
-  #become zombie
 
-
-
-
-end
 
 def outrun_zombie?
   your_speed_atm= rand(@@max_speed)
@@ -76,21 +72,39 @@ def self.all
 
 end
 
+
 def self.new_day
-  #call some_die_off
   #call spawn
+  Zombie.spawn
+
+  #call some_die_off
+  Zombie.some_die_off
+
   #call increase_plague_level
+  Zombie.increase_plague_level
   #these methods will interact with each other to represent a new day in the apocalyspe
 
 
 end
 
+#what if we have less than the number 10 in the horde?
 def self.some_die_off
 #generate a random number from 0 to 10
-some_die = rand(11)
+  some_die = rand(11)
+
+  puts "This is the number of zombies that die off #{some_die}"
 #remove that number from the array of zombies in the horde
+  array = []
 
+  array = @@horde.sample(some_die)
 
+  array.each do |zombie|
+    @@horde.delete(zombie)
+  end
+
+  puts @@horde
+
+#COME BACK
 end
 
 def self.spawn
@@ -100,7 +114,7 @@ def self.spawn
       zombie = Zombie.new(rand(@@max_speed), rand(@@max_strength))
       @@horde << zombie
     end
-
+end
 
 
   #create that number of zombies
@@ -109,16 +123,61 @@ def self.spawn
   #add each one to horde
 
 
-end
+
 
 def self.increase_plague_level
   #increase the plague level
   increase_level = rand(3)
-  @@plague_level += level
-
+  @@plague_level += increase_level
+  puts "NEW PLAGUE LEVEL"
+  puts @@plague_level
 end
 
 
 
 
 end
+
+
+
+
+
+puts Zombie.all.inspect
+puts Zombie.spawn.inspect
+puts "!!!!!"
+puts Zombie.all.inspect
+puts Zombie.some_die_off
+puts "AFTER DEATH"
+puts Zombie.all.inspect
+
+puts "NEW DAY"
+puts Zombie.new_day
+puts Zombie.spawn
+puts Zombie.all.inspect
+
+puts "&&&&&&&"
+
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+puts zombie1.encounter # You got away
+puts zombie2.encounter # You are now a zombie
+puts zombie3.encounter
+
+=begin
+puts Zombie.all.inspect # [#<Zombie:0x005626ecc5ebd0 @speed=4, @strength=0>, #<Zombie:0x005626ecc5eba8 @speed=0, @strength=4>, #<Zombie:0x005626ecc5eb80 @speed=4, @strength=4>]
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+puts zombie1.encounter # You are now a zombie
+puts zombie2.encounter # You died
+puts zombie3.encounter # You died
+Zombie.new_day
+puts Zombie.all.inspect # [#<Zombie:0x005626ecc5e1f8 @speed=0, @strength=0>, #<Zombie:0x005626ecc5e180 @speed=3, @strength=3>, #<Zombie:0x005626ecc5e158 @speed=1, @strength=2>, #<Zombie:0x005626ecc5e090 @speed=0, @strength=4>]
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+puts zombie1.encounter # You got away
+puts zombie2.encounter # You are now a zombie
+puts zombie3.encounter
+=end
